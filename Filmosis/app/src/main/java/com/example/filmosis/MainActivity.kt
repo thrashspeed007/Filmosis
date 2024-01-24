4d8b0157c759e38b9e11d344e0e870483a8553f1
Adrián <agarciagale@gmail.com>
2024-01-24 21:03:41 +0100

    mas cosas

--- Filmosis/app/src/main/java/com/example/filmosis/MainActivity.kt
+++ Filmosis/app/src/main/java/com/example/filmosis/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.widget.TextView
 import com.example.filmosis.config.DatosConexion
 import com.example.filmosis.data.FilmosisApiInterface
+import com.example.filmosis.data.TmdbApiInterface
 import com.example.filmosis.data.UsuarioItem
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getUsuarios()
+    }
+
+    private fun listPopularMovies() {
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl(DatosConexion.TMDB_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TmdbApiInterface::class.java)
     }
 
     private fun getUsuarios() {
--- Filmosis/app/src/main/java/com/example/filmosis/data/TmdbApiInterface.kt
+++ Filmosis/app/src/main/java/com/example/filmosis/data/TmdbApiInterface.kt
@@ -1,4 +1,13 @@
 package com.example.filmosis.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TmdbApiInterface {
+    @GET("discover/movie?sort_by=popularity.desc")
+    fun listPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    )
 }
