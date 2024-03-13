b4aad0fa11806b29d8fd034239e988b1b5e7f9da
Adrián <agarciagale@gmail.com>
2024-03-13 19:05:06 +0100

    un test

--- Filmosis/app/build.gradle.kts
+++ Filmosis/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    debugImplementation("androidx.fragment:fragment-testing:1.6.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
